c6e7b7d22970d872dd8cdc97ea70bc8715d9070f
Gorlaks <gorlakswork@gmail.com>
2021-06-30 13:30:29 +0300

    drawer

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,12 +3,17 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') },
+      { path: '', component: () => import('pages/Home.vue') },
+    ],
+  },
+  {
+    path: '/events',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Events.vue') },
     ],
   },
 
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '*',
     component: () => import('pages/Error404.vue'),
